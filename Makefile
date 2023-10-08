591d96dcc71d44934cc31d984c8c5e791ead3c51
Jon-Michael Hartway <jhartway99@gmail.com>
2023-10-08 13:16:38 -0400

    basic bot working

--- Makefile
+++ Makefile
@@ -5,11 +5,13 @@ LIBS = -ldpp
 BUILD_DIR = build
 SRC_DIR = src
 INCLUDE_DIR = /usr/local/include
+LIB_DIR = /usr/local/lib
 
 
+export LD_LIBRARY_PATH=/usr/local/lib
 
 build:
-	$(CC) $(SRC_DIR)/*.cpp -I $(INCLUDE_DIR) $(CFLAGS) $(LIBS) -o $(BUILD_DIR)/main.o
+	$(CC) $(SRC_DIR)/*.cpp -I $(INCLUDE_DIR) $(CFLAGS) -L$(LIB_DIR) $(LIBS) -o $(BUILD_DIR)/main.o
 
 run:
 	make build
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <dpp/dpp.h>
-
+#include "secrets.hpp"
 
 
 int main()
 {
+    dpp::cluster bot(DISCORD_APPLICATION_TOKEN);
 
+    bot.on_slashcommand([](auto event) {
+        if(event.command.get_command_name() == "ping") {
+            event.reply("Pong!");
+        }
+
+    });
+
+    bot.on_ready([&bot](auto event) {
+        if(dpp::run_once<struct register_bot_commands>()){
+          bot.global_command_create(
+              dpp::slashcommand("ping", "Ping the bot",bot.me.id)
+          );
+        }
+    });
+
+
+    bot.start(dpp::st_wait);
     std::cout << "Hello World!\n";
     return 0;
 }
